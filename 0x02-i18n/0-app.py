4f910e9167bd63491fe2d779e7719e933cfe5e03
Adam <adamelmadani0129@gmail.com>
2024-02-10 10:33:49 +0000

    0.task

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This is our python module
+"""
+from flask import Flask, render_template
+"""
+These are flask and flask babel modules
+"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,1 @@
+These are i18n tasks
